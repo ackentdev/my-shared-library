9b6976fbc45142b3b460c8d6f135888f1bbc411a
Adam Kent <Adam@Adams-MacBook-Pro-2.local>
2019-08-10 09:48:59 -0700

    modified add sql

--- db/add_concert.sql
+++ db/add_concert.sql
@@ -1,2 +1,7 @@
 insert into concerts(user_id, concert_name, date, song_id)
 values($1, $2, $3, $4);
+
+select c.concert_id, c.concert_name, c.date, sl.song_name, sl.catalog_id from concerts c
+join song_library sl
+on (sl.song_id = c.song_id)
+where c.user_id = $1;
